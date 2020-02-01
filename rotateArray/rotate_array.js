53c5c312c6c6fdd05d0ed4e73721fe227e1ddfe1
Peter Szocs <gepesz@gmail.com>
2020-02-01 18:34:30 -0500

    Rotate a sorted array by k steps

--- rotateArray/rotate_array.js
+++ rotateArray/rotate_array.js
@@ -1,8 +1,12 @@
 /**
- Rotate an array of n elements to the right by k steps.
-
- For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated
- to [5,6,7,1,2,3,4]. 
+ * LEETCODE 189: Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/
+ *
+ * Rotate an array of n elements to the right by k steps.
+ *
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated
+ * to [5,6,7,1,2,3,4].
  */
 function rotateArray(arr, k) {
   // what we return
