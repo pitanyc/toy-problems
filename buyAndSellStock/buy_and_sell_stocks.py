8800f9ae30feafa50c94ea369d40f81a829967af
Peter Szocs <gepesz@gmail.com>
2020-02-01 17:59:44 -0500

    Best time to buy and sell stock

--- buyAndSellStock/buy_and_sell_stocks.py
+++ buyAndSellStock/buy_and_sell_stocks.py
@@ -1,3 +1,7 @@
+# LEETCODE 121: Best Time to Buy and Sell Stock
+#
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+#
 # Say you have an array for which the ith element is the price of a given stock on day i.
 # 
 # If you were only permitted to complete at most one transaction 
@@ -13,8 +17,7 @@
 #
 # Input: [7,6,4,3,1]
 # Output: 0
-#
-# 121: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
 def max_profit(prices):
     # sanity checks
     count = len(prices)
