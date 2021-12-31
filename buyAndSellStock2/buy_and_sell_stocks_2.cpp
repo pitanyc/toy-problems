71e374c390229e907ce9a3146a3339d328bd85fd
Peter Szocs <pszocs@bloomberg.net>
2021-12-31 15:13:05 -0800

    Best time to buy and sell stocks, multiple transactions

--- buyAndSellStock2/buy_and_sell_stocks_2.cpp
+++ buyAndSellStock2/buy_and_sell_stocks_2.cpp
@@ -17,31 +17,34 @@
  *
  * Input: [7,1,5,3,6,4]
  * Output: 7
- * 
+ *
  * Example 2:
  *
  * Input: [1,2,3,4,5]
  * Output: 4
  */
 
+#include <algorithm> // std::find
 #include <iostream>
 #include <map>
 #include <vector>
 
 using namespace std;
 
-std::ostream &operator<<(std::ostream &os, const std::vector<int> arr)
+std::ostream& operator<<(std::ostream& os, const std::vector<int> arr)
 {
-    for (std::vector<int>::const_iterator it = arr.begin();
-         it != arr.end();
-         it++)
+    for ( std::vector<int>::const_iterator it = arr.begin(); it != arr.end();
+          it++ )
     {
         os << *it << " ";
     }
     return os;
 }
 
-// SOLTION 1: O(n) solution by consuming input prices
+// SOLTION: Find local minimum & maximum points.
+//          Iterate thru prices and calculate profit between those points.
+// Time: O(N)
+// Space: O(N)
 int maxProfit(const vector<int>& prices)
 {
     // what we return
@@ -49,32 +52,74 @@ int maxProfit(const vector<int>& prices)
 
     // locals
     unsigned int count = prices.size();
-    if (count == 0)
+    if ( count < 2 )
     {
         return returnValue;
     }
 
-    // we have at least 1 element in the array ==> consume input from the right
-    int maxSoFar = 0;
-    for (int i = count -1; i >= 0; i--)
+    // we have at least 2 prices ==> find local minimum and maximum points
+
+    // use: 1 = local minimum, 2 = local maximum
+    vector<int> locals;
+    locals.resize(count); // resize inits all elements to 0 automatically
+
+    // first and last elements
+    if ( prices.at(0) < prices.at(1) )
     {
-        // check if this is a bigger max
-        if ( prices[i] > maxSoFar )
+        locals.at(0) = 1;
+    }
+    if ( prices.at(count - 2) < prices.at(count - 1) )
+    {
+        locals.at(count - 1) = 2;
+    }
+
+    // middle elements
+    for ( unsigned int i = 1; i < count - 1; i++ )
+    {
+        int current = prices.at(i);
+        int prev = prices.at(i - 1);
+        int next = prices.at(i + 1);
+
+        if ( prev > current && current < next )
         {
-            maxSoFar = prices[i];
+            // local min
+            locals.at(i) = 1;
         }
-        returnValue += maxSoFar - prices[i];
-        std::cout << "prices[i] = " << prices[i] << ", maxSoFar = " << maxSoFar << ", maxSoFar - prices[i] = " << (maxSoFar - prices[i]) << std::endl;
+        else if ( prev < current && current > next )
+        {
+            // local max
+            locals.at(i) = 2;
+        }
+    }
+    std::cout << "locals: " << locals << std::endl;
+
+    // simply count the profit between local min & max points
+    for ( unsigned int i = 0; i < count; i++ )
+    {
+        if ( locals.at(i) == 1 )
+        {
+            // this is a local minimum ==> find corresponding local max
+            for ( unsigned int j = i + 1; j < count; j++ )
+            {
+                if ( locals.at(j) == 2 )
+                {
+                    // this is the next local max, calculate profit
+                    returnValue += prices.at(j) - prices.at(i);
+                    i = j;
+                    break;
+                }
+            }
+        }
     }
 
     // finally return
     return returnValue;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     // test case 1
-    std::vector<int> input = {7,1,5,3,6,4};
+    std::vector<int> input = {7, 1, 5, 3, 6, 4};
     std::cout << "input: " << input << std::endl;
     int output = maxProfit(input);
     std::cout << "maxProfit: " << output << std::endl;
@@ -82,6 +127,13 @@ int main(int argc, char const *argv[])
     // 2
     std::cout << "\n====\n" << std::endl;
     input = {1, 5, 2, 3, 5};
+    std::cout << "input: " << input << std::endl;
+    output = maxProfit(input);
+    std::cout << "maxProfit: " << output << std::endl;
+
+    // 3
+    std::cout << "\n====\n" << std::endl;
+    input = {1, 2, 3, 4, 5};
     std::cout << "input: " << input << std::endl;
     output = maxProfit(input);
     std::cout << "maxProfit: " << output << std::endl;
